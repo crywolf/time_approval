17bd053759702d09a0f2a43c1932ec46b7f90cba
CryWolf <cry.wolf@centrum.cz>
2018-02-06 18:57:34 +0100

    Approved time entry is not bulk editable

--- lib/timelog_controller_patch.rb
+++ lib/timelog_controller_patch.rb
@@ -4,17 +4,20 @@ module TimelogControllerPatch
 
     base.class_eval do
       alias_method_chain :update, :check_if_approved
+      alias_method_chain :bulk_update, :check_if_approved
     end
 
   end
 
   module InstanceMethods
     def update_with_check_if_approved
-      if @time_entry.approved
-        render_403
-        return false
-      end
+      raise Unauthorized if @time_entry.approved?
       update_without_check_if_approved
+    end
+
+    def bulk_update_with_check_if_approved
+      raise Unauthorized if @time_entries.any? {|t| t.approved?}
+      bulk_update_without_check_if_approved
     end
   end
 end
