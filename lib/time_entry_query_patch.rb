8b9084956a0a457db47090750d001a0f0d90cb8a
CryWolf <cry.wolf@centrum.cz>
2018-02-22 11:32:16 +0100

    Adds possibility to filter time entries by issue

--- lib/time_entry_query_patch.rb
+++ lib/time_entry_query_patch.rb
@@ -14,6 +14,7 @@ module TimeEntryQueryPatch
   module InstanceMethods
     def initialize_available_filters_with_field_approved
       initialize_available_filters_without_field_approved
+      add_available_filter "issue_id", type: :integer, label: :label_issue
       add_available_filter "approved", type: :list, values: [[l('general_text_No'), "false"], [l('general_text_Yes'), "true"]]
     end
   end
